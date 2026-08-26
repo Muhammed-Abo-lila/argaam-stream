06dcdf9cfbc65a0a272dd72eae088be141617ca4
muhammed abo lila <mohamed.abolila@argaam.com>
2026-08-26 23:42:37 +0300

    feat: - initialize React project with Vite - install required libraries, including Bootstrap and i18n - test Bootstrap integration - remove unnecessary files and code

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="container">
+      <p type="button" className="text-success" aria-label="Close">test text</p>
+    </div>
+  )
+}
+
+export default App
