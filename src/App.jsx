4a086752aed4ea56930b31b4c3e7e90222bd3463
muhammed abo lila <mohamed.abolila@argaam.com>
2026-08-26 23:47:20 +0300

    feat: implement internationalization with language switching functionality and test it

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import { useTranslation } from "react-i18next";
+import useLang from "./utils/useLang";
+
 function App() {
+  const { i18n } = useTranslation();
+  const changeLanguage = (language) => {
+    i18n.changeLanguage(language);
+  };
+  const lang = useLang("en", "ar");
   return (
     <div className="container">
-      <p type="button" className="text-success" aria-label="Close">test text</p>
+      <h1 className="theme_text_color">{lang === "en" ? "hello" : "مرحبا"}</h1>
+      <button className="btn btn-info" onClick={() => changeLanguage("en")}>
+        {lang === "en" ? "en" : "الانجليزيه"}
+      </button>
+      <button className="btn btn-primary" onClick={() => changeLanguage("ar")}>
+        {lang === "en" ? "ar" : "العربيه"}
+      </button>
     </div>
   )
 }
