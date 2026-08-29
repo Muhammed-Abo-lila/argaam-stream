077bb55f15a9189ddfcf902b0e450d247bd7c3be
muhammed abo lila <mohamed.abolila@argaam.com>
2026-08-29 12:45:18 +0300

    feat: add footer component and update font sizes
    - Add Footer component with Arabic and English content
    - Add custom font size variables and utility classes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -2,17 +2,20 @@ import React from "react";
 import { useTranslation } from "react-i18next";
 import { Outlet, useNavigate } from "react-router-dom";
 import useLang from "./utils/useLang";
+import Footer from "./components/Footer";
+import { useTheme } from "./context/ThemeContext";
 
 const Layout = () => {
   const lang = useLang("en", "ar");
   const navigate = useNavigate();
-    const { i18n } = useTranslation();
-    const changeLanguage = (language) => {
-        const pathParts = location.pathname.split("/");
-        pathParts[1] = language;
-        i18n.changeLanguage(language);
-        navigate(pathParts.join("/"));
-    };
+  const { i18n } = useTranslation();
+  const changeLanguage = (language) => {
+    const pathParts = location.pathname.split("/");
+    pathParts[1] = language;
+    i18n.changeLanguage(language);
+    navigate(pathParts.join("/"));
+  };
+  const { theme, toggleTheme } = useTheme()
   return (
     <>
       <button
@@ -21,7 +24,15 @@ const Layout = () => {
       >
         {lang === "en" ? "lang" : "اللغه"}
       </button>
+
+      <button
+        className="btn btn-info"
+        onClick={() => toggleTheme(!theme)}
+      >
+        {theme === "light" ? "dark" : "light"}
+      </button>
       <Outlet />
+      <Footer />
     </>
   );
 };
